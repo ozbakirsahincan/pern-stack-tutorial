fde2607d8658a1a0da82096e249085e890fb7abb
ozbakirsahincan <ozbakirsahincan@gmail.com>
2025-04-16 01:26:44 +0200

    database switch neon -> local postgresql

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,16 @@
-import { neon } from "@neondatabase/serverless";
+// import { neon } from "@neondatabase/serverless";
+import postgres from "postgres";
 import dotenv from "dotenv";
 dotenv.config();
 
-const {PGHOST , PGUSER, PGPASSWORD, PGDATABASE,} = process.env;
-const connectionString = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`;
 
-// Create sql connection from env
-export const sql = neon(connectionString);
+const {PGHOST , PGUSER, PGPASSWORD, PGDATABASE,} = process.env;
+const connectionString = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}`;
+
+
+export const sql = postgres(connectionString, {
+    host: PGHOST,
+    user: PGUSER,
+    password: PGPASSWORD,
+    database: PGDATABASE,
+})
